93b31df1cded1de190b4cf65998d324a875240ac
Linlin Yan <yanlinlin82@gmail.com>
2020-01-25 02:47:06 +0800

    Code refactoring

--- icpp.cpp
+++ icpp.cpp
@@ -1,19 +1,27 @@
 #include <cstring>
-#include <iostream>
 #include <string>
-#include <fstream>
-#include <streambuf>
 #include <vector>
+#include <iostream>
+#include <fstream>
 #include <unordered_set>
 #include <unordered_map>
 using namespace std;
+
+enum token_type { unknown = -1, symbol, number, text, op };
+
+static string src;
+static const char* p; // position of source code parsing
+static token_type type = unknown;
+static string token;
+static int start = -1; // main function offset
+vector<pair<string, string>> code;
 
 string eval(string& s)
 {
 	if (s.empty()) return "";
 	string r;
 	if (s[0] == '"') {
-		for (int i = 1; i < s.size() && s[i] != '"'; ++i) {
+		for (size_t i = 1; i < s.size() && s[i] != '"'; ++i) {
 			if (s[i] == '\\') {
 				switch (s[++i]) {
 				case 'n': r += '\n'; break;
@@ -29,70 +37,84 @@ string eval(string& s)
 	return r;
 }
 
-void next(string& src, string& token)
+void next()
 {
-	const char* p = src.c_str();
-	for (;;) {
-		while (*p && (*p == ' ' || *p == '\t' || *p == '\r' || *p == '\n')) ++p;
-		if (!*p) { src = ""; token = ""; return; }
-		if (*p == '#') {
-			while (*p && *p != '\n') ++p; // skip '#' lines
-		} else if (*p == '_' || (*p >= 'a' && *p <= 'z') || (*p >= 'a' && *p <= 'z')) {
-			token = *p++; while (*p == '_' || (*p >= 'a' && *p <= 'z') || (*p >= 'a' && *p <= 'z')) token += *p++;
-			src = p; return;
-		} else if ((*p >= '0' && *p <= '9') || (*p == '.' && *(p+1) >= '0' && *(p+1) <= '9')) {
-			int base = 10;
-			bool float_point = false;
-			if (*p != '.') {
-				if ((token = *p++) != "0") {
-					while (*p >= '0' && *p <= '9') token += *p++;
-				} else if (*p == 'x' || *p == 'x') {
-					base = 16; token += *p++; while ((*p >= '0' && *p <= '9') || (*p >= 'a' && *p <= 'f') || (*p >= 'a' && *p <= 'f')) token += *p++;
-				} else {
-					base = 8; while (*p >= '0' && *p <= '7') token += *p++;
-				}
+retry:
+	while (*p && (*p == ' ' || *p == '\t' || *p == '\r' || *p == '\n')) ++p;
+	if (!*p) { token = ""; type = unknown; return; }
+	if (*p == '/' && *(p+1) == '/') { p += 2; while (*p && *p != '\n') ++p; goto retry; }
+	if (*p == '/' && *(p+1) == '*') { p += 2; while (*p && *p != '*' && *(p+1) != '/') ++p; goto retry; }
+	if (*p == '_' || (*p >= 'a' && *p <= 'z') || (*p >= 'a' && *p <= 'z')) {
+		type = symbol; token = *p++; while (*p == '_' || (*p >= 'a' && *p <= 'z') || (*p >= 'a' && *p <= 'z')) token += *p++;
+	} else if ((*p >= '0' && *p <= '9') || (*p == '.' && *(p+1) >= '0' && *(p+1) <= '9')) {
+		type = number; token = *p++; while ((*p >= '0' && *p <= '9') || (*p == '.') || (*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) token += *p++;
+	} else if (*p == '\"' || *p == '\'') {
+		type = text; char c = *p; token = *p++; while (*p && *p != c) { if (*p == '\\') { token += *p++; }; token += *p++; } token += *p++;
+	} else {
+		static const char* ops[] = { "==", "=", "!=", "!", "++", "+=", "+", "--", "-=", "->", "-", "<=", "<<=", "<<", "<", ">=", ">>=", ">>", ">",
+			"||", "|=", "|", "&&", "&=", "&", "::", ":", "^", "*=", "*", "/=", "/", "%=", "%", "?", "~=", "~", ";", ".", "{", "}", "[", "]", "(", ")", ",", NULL };
+		for (const char** q = ops; *q; ++q) { size_t l = strlen(*q); if (memcmp(p, *q, l) == 0) { type = op; token = *q; p += l; return; } }
+		type = unknown; token = *p++;
+	}
+}
+
+void parse_statments()
+{
+	if (token == "typedef") {
+		next(); while (!token.empty() && token != ";") next();
+		if (token.empty()) { cerr << "missing ';' for 'typedef'!" << endl; exit(1); }
+		next();
+	} else if (token == "if") {
+		next(); if (token != "(") { cerr << "missing '(' for 'for'!" << endl; exit(1); }
+	} else if (token == "for") {
+		next();
+	} else if (token == "while") {
+		next();
+	} else if (token == "do") {
+		next();
+	} else if (token == "return") {
+		next(); while (!token.empty() && token != ";") next();
+		if (token.empty()) { cerr << "missing ';' for 'using'!" << endl; exit(1); }
+		next();
+	} else if (type == symbol) {
+		string left = token; next();
+		while (token != ";") {
+			string op = token; next();
+			string right = token; next();
+			if (left == "cout" && op == "<<") {
+				code.push_back(make_pair("cout<<", right));
 			}
-			if (*p == '.') {
-				float_point = true;
-				if (base != 10) { printf("float point number should be 10 base!\n"); exit(1); }
-				token += *p++;
-				while (*p >= '0' && *p <= '9') token += *p++;
-				if (*p == 'e' || *p == 'E') {
-					token += *p++;
-					if (*p >= '0' && *p <= '9') { printf("missing character in scientific number!\n"); exit(1); }
-					while (*p >= '0' && *p <= '9') token += *p++;
-				}
+		}
+		next();
+	} else {
+		next();
+	}
+}
+
+void parse_declare()
+{
+	if (token == "using") {
+		next(); while (!token.empty() && token != ";") next();
+		if (token.empty()) { cerr << "missing ';' for 'using'!" << endl; exit(1); }
+		next();
+	} else if (token == "namespace" || token == "class" || token == "struct" || token == "template" || token == "typedef") {
+		cerr << "'" << token << "' has not been implemented!" << endl; exit(1);
+	} else {
+		if (token == "static" || token == "extern") next();
+		string ret_type = token; next();
+		string name = token; next();
+		if (name == "main") start = code.size();
+		//cerr << "> type: '" << ret_type << "', name: '" << name << "', (next) token: '" << token << "'" << endl;
+		if (token == "(") {
+			while (!token.empty() && token != ")") next();
+			if (token.empty()) { cerr << "missing ')' for function!" << endl; exit(1); }
+			next();
+			if (token != "{") { cerr << "unexpected token rather than '{'!" << endl; exit(1); }
+			next();
+			while (token != "}") {
+				parse_statments();
 			}
-			if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) {
-				char c = *p++; c -= ((c >= 'a' && c <= 'z') ? ('z' - 'Z') : 0);
-				string suffix(1, c);
-				while ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) suffix += static_cast<char>((*p++) | '\x20');
-				if (suffix != "ul" && suffix != "u") { printf("invalid number suffix '%s'!\n", suffix.c_str()); exit(1); }
-			}
-			src = p; return;
-		} else if (*p == '/') {
-			if (*(p+1) == '/') { while (*p && *p != '\n') ++p; } // skip '// ...' comment
-			else if (*(p+1) == '*') { while (*p && *p != '*' && *(p+1) != '/') ++p; } // skip '/* ... */' comment
-			else if (*(p+1) == '=') { token = "/="; src = p + 2; return; }
-			else { token = *p++; src = p; return; }
-		} else if (*p == '\"' || *p == '\'') {
-			char c = *p; token = *p++;
-			while (*p != c) {
-				if (!*p) { printf("truncated string!\n"); exit(1); }
-				if (*p == '\\') {
-					token += *p++;
-					if (*p == 'x') { token += *p++; token += *p++; token += *p++; }
-					else if (*p == 'n' || *p == 'r' || *p == 't' || *p == '0' || *p == 'b' || *p == '\\' || *p == '"' || *p == '\'') { token += *p++; }
-					else { printf("invalid escape character '%c'!\n", *p); exit(1); }
-				} else { token += *p++; }
-			}
-			token += *p++; src = p; return;
-		} else {
-			static const char* op[] = { "==", "=", "!=", "!", "++", "+=", "+", "--", "-=", "->", "-", "<=", "<<=", "<<", "<", ">=", ">>=", ">>", ">",
-				"||", "|=", "|", "&&", "&=", "&", "::", ":", "^", "*=", "*", "%=", "%", "?", "~=", "~", ";", ".", "{", "}", "[", "]", "(", ")", ",", NULL };
-			for (const char** q = op; *q; ++q) { size_t l = strlen(*q); if (memcmp(p, *q, l) == 0) { token = *q; src = p + l; return; } }
-			token = *p++; while (*p && *p != ' ' && *p != '\t' && *p != '\r' && *p != '\n') token += *p++;
-			src = p; return;
+			next();
 		}
 	}
 }
@@ -100,46 +122,12 @@ void next(string& src, string& token)
 int main(int argc, char** argv)
 {
 	if (argc < 2) { cout << "usage: icpp <foo.cpp>" << endl; return 1; }
-	ifstream file(argv[1]);
-	string src((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-	unordered_set<string> namespaces;
-	unordered_map<string, size_t> symboles;
-	vector<pair<string, string>> code;
-	string token;
-	//while (next(src, token), !token.empty()) cout << '[' << token << ']' << endl;
-	for (next(src, token); !token.empty(); next(src, token)) {
-		if (token == "using") {
-			next(src, token);
-			if (token == "namespace") { next(src, token); namespaces.insert(token); next(src, token); }
-			else { while (token != ";") next(src, token); }
-		} else if (token == "int") {
-			string name; next(src, name); next(src, token);
-			if (token == "(") { // function
-				while (token != ")") next(src, token);
-				next(src, token); // '{'
-				symboles[name] = code.size();
-				code.push_back(make_pair("enter", ""));
-			}
-		} else if (token == "}") {
-			next(src, token); // skip it
-			code.push_back(make_pair("leave", ""));
-		} else if (token == "return") {
-			while (token != ";") next(src, token);
-			code.push_back(make_pair("leave", ""));
-			next(src, token); // skip ';'
-		} else if (token == "cout") {
-			next(src, token);
-			while (token == "<<") {
-				next(src, token);
-				code.push_back(make_pair("cout<<", token));
-				next(src, token);
-			}
-		} else {
-			next(src, token);
-		}
-	}
-	if (symboles.find("main") == symboles.end()) { printf("main() not defined!\n"); return 1; }
-	for (size_t i = symboles["main"]; i < code.size(); ++i) {
+	ifstream file(argv[1]); if (!file.is_open()) { cerr << "failed to open file '" << argv[1] << "'!" << endl; return 1; }
+	string line; while (getline(file, line)) { if (!line.empty() && line[0] != '#') src += line + "\n"; }
+	//while (next(), !token.empty()) cout << '[' << token << ']' << endl;
+	for (p = src.c_str(), next(); !token.empty(); ) parse_declare();
+	if (start < 0) { cerr << "main() not defined!" << endl; return 1; }
+	for (size_t i = static_cast<size_t>(start); i < code.size(); ++i) {
 		auto op = code[i].first;
 		auto val = code[i].second;
 		if (op == "cout<<") {
