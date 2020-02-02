f433982d64a843d943f17e2e5183b0841505d492
Linlin Yan <yanlinlin82@gmail.com>
2020-02-02 12:33:20 +0800

    Update test examples

---
+++ tests/005-operator.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+	int a = 123, b = 456, c;
+	c = a + b;
+	c = a - b;
+	c = a * b;
+	c = a / b;
+	c = a % b;
+	c = a >> b;
+	c = a << b;
+	c = a & b;
+	c = a | b;
+	c = a == b;
+	c = a != b;
+	c = a >= b;
+	c = a > b;
+	c = a <= b;
+	c = a < b;
+	c = a && b;
+	c = a || b;
+	return 0;
+}
