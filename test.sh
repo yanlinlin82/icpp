8c1d009cde3433b2dca916e2ed6c08b07c2e5d60
Linlin Yan <yanlinlin82@gmail.com>
2020-02-04 08:22:32 +0800

    Move md5sum files to sub-dir

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-for f in tests/*.cpp; do echo $f; ./icpp $f | md5sum -c ${f/cpp/md5sum} || exit 1; echo; done; echo "all passed."
+ls tests/ | grep '\.cpp$' | while read f; do echo tests/$f; ./icpp tests/$f | md5sum -c tests/md5sum/${f%.cpp}.md5sum || exit 1; done; echo "all passed."
