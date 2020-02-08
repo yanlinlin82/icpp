ff7c0529ba3e05d82ed332b2cf85ef6928eb2d6f
Linlin Yan <yanlinlin82@gmail.com>
2020-02-08 09:34:18 +0800

    Fix bash script

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-ls tests/ | grep '\.cpp$' | while read f; do echo tests/$f; ./icpp tests/$f | md5sum -c tests/md5sum/${f%.cpp}.md5sum || exit 1; done; echo "all passed."
+#!/bin/bash
+set -e
+ls tests/ | grep '\.cpp$' | while read f; do
+	echo tests/$f
+	./icpp tests/$f | md5sum -c tests/md5sum/${f%.cpp}.md5sum
+done
+echo "all passed."
